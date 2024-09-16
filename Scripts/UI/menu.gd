d66a59bb852ebf25350761a5af945425f3c38aaa
Wickyr <parsonsj689@gmail.com>
2024-09-16 12:16:15 -0500

    Menu

--- Scripts/UI/menu.gd
+++ Scripts/UI/menu.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var menu: VBoxContainer = $MarginContainer/menu
+@onready var settings: VBoxContainer = $MarginContainer/settings
 
 
 func _on_play_pressed():
@@ -6,8 +8,14 @@ func _on_play_pressed():
 
 
 func _on_setting_pressed():
-	pass # Replace with function body.
+	settings.visible = true
+	menu.visible = false
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	settings.visible = false
+	menu.visible = true
