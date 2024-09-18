3dffb0cf679833e0acf69d3f49049226ab124690
Jason <haruspexending@gmail.com>
2024-09-18 16:16:34 -0400

    TrufflePickUpAudio

--- Scripts/ray_cast_3d.gd
+++ Scripts/ray_cast_3d.gd
@@ -3,5 +3,6 @@ extends RayCast3D
 func _process(delta):
 	if is_colliding():
 		var hitObj = get_collider()
-		if hitObj.has_method("interact") && Input.is_action_just_pressed("e"):
-			hitObj.interact()
+		if hitObj and hitObj.has_method("interact"): 
+			if Input.is_action_just_pressed("e"):
+				hitObj.interact()
--- Scripts/truffle.gd
+++ Scripts/truffle.gd
@@ -2,7 +2,13 @@ extends CSGBox3D
 
 var toggle = true
 var interactable = true
-@onready var truffle: CSGBox3D = $"."
+
+@onready var truffle: CSGBox3D = $"." 
+@onready var sound_player: AudioStreamPlayer3D = $TrufflePickUp 
 
 func interact():
-	truffle.queue_free()
+	if interactable:
+		truffle.visible = false  
+		sound_player.play()  
+		await get_tree().create_timer(1.0).timeout  
+		truffle.queue_free() 
